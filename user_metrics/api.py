9fde98a5e34db95052e2964f7f16fbd4dc562ba1
Alexander Olekhnovich <alexander.olehnovich@viaden.com>
2015-06-15 17:50:44 +0300

    Remove hardcoded stuff PEP8 changes

--- user_metrics/api.py
+++ user_metrics/api.py
@@ -1,45 +1,37 @@
+from django.conf import settings
 from user_metrics.models import Metric, MetricItem
 from user_metrics.utils import get_visitor_hash, create_visitor_id
 
-from django.http import HttpResponseRedirect
-
 import datetime
 
-VISITOR_ID_COOKIE = 'x-Arendorium-ID'
+VISITOR_ID_COOKIE_NAME = getattr(settings, 'VISITOR_ID_COOKIE', 'x-Arendorium-ID')
+
 
 def put_metric(slug, user, user_object_id=0, request=None, response=None, cookie_domain=None, **kwargs):
     """ Increment a metric by a given user """
 
-    try:
-        metric = Metric.objects.get(slug=slug)
-    except Metric.DoesNotExist:
-        metric = Metric.objects.create(slug=slug, name=slug)
+    metric, created = Metric.objects.get_or_create(slug=slug, defaults={'name': slug})
 
     visitor_hash = None
     visitor_id = None
 
     if request is not None and response is not None:
-        # NOTE: expiring old cookies; the following loop can be removed in future
-        for name, value in request.COOKIES.iteritems():
-            if name.startswith('x-Arendorium-') and name != VISITOR_ID_COOKIE:
-                response.set_cookie(name, '', expires=datetime.date(2010, 1, 1), domain=cookie_domain)
-
         visitor_hash = get_visitor_hash(request)
 
-        visitor_id = request.COOKIES.get(VISITOR_ID_COOKIE, None)
+        visitor_id = request.COOKIES.get(VISITOR_ID_COOKIE_NAME, None)
         if visitor_id is None:
             visitor_id = create_visitor_id(request, visitor_hash)
             utcnow = datetime.datetime.utcnow()
             expires = utcnow.replace(year=utcnow.year+1)
 
-            response.set_cookie(VISITOR_ID_COOKIE, value=visitor_id, expires=expires, domain=cookie_domain)
+            response.set_cookie(VISITOR_ID_COOKIE_NAME, value=visitor_id, expires=expires, domain=cookie_domain)
 
     MetricItem.objects.create(
-        metric = metric,
-        user = user,
-        user_object_id = user_object_id,
-        visitor_hash = visitor_hash,
-        visitor_id = visitor_id
+        metric=metric,
+        user=user,
+        user_object_id=user_object_id,
+        visitor_hash=visitor_hash,
+        visitor_id=visitor_id
     )
 
     return response
--- user_metrics/management/commands/metrics_aggregate_by_user.py
+++ user_metrics/management/commands/metrics_aggregate_by_user.py
@@ -12,17 +12,16 @@ class Command(NoArgsCommand):
     def handle_noargs(self, **options):
         """ Aggregate Metrics by User """
 
-        items = MetricItem.objects.extra(
-                select={'day': "DATE_TRUNC('day', visit_time)"}
-            ).values('day', 'metric', 'user', 'user_object_id').order_by().annotate(
-                    count=Count('id'), unique_count=Count('visitor_id', distinct=True))
+        items = MetricItem.objects.extra(select={'day': "DATE_TRUNC('day', visit_time)"}).\
+            values('day', 'metric', 'user', 'user_object_id').order_by().annotate(
+            count=Count('id'), unique_count=Count('visitor_id', distinct=True))
 
         for item in items:
             day, create = MetricDay.objects.get_or_create(
-                date_up = item['day'].date(),
-                metric_id = item['metric'],
-                user_id = item['user'],
-                user_object_id = item['user_object_id']
+                date_up=item['day'].date(),
+                metric_id=item['metric'],
+                user_id=item['user'],
+                user_object_id=item['user_object_id']
             )
 
             day.count = item['count']
